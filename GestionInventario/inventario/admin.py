df50b9993b503aa97aeaea229f2e08422c1468e9
aurbanoc94 <avalonuc@gmail.com>
2024-12-28 09:09:17 -0500

    registro de los modelos

--- GestionInventario/inventario/admin.py
+++ GestionInventario/inventario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria, Proveedor, Producto, Cliente, Orden, DetalleOrden
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Proveedor)
+admin.site.register(Producto)
+admin.site.register(Cliente)
+admin.site.register(Orden)
+admin.site.register(DetalleOrden)
